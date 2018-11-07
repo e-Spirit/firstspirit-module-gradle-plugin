3386ca2a396c855bc40fdbd376cdcfb8ce4e500b
Emre Izgoel <izgoel@e-spirit.com>
2018-11-07 15:18:02 +0100

    DEVEX-220 added class that implements de.espirit.firstspirit.module to test-project

---
+++ test-project/fsm/src/main/java/org/example/SimpleModule.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import de.espirit.firstspirit.module.Module;
+import de.espirit.firstspirit.module.ServerEnvironment;
+import de.espirit.firstspirit.module.descriptor.ModuleDescriptor;
+
+public class SimpleModule implements Module{
+
+    @Override
+    public void init(ModuleDescriptor moduleDescriptor, ServerEnvironment serverEnvironment) {
+
+    }
+
+    @Override
+    public void installed() {
+
+    }
+
+    @Override
+    public void uninstalling() {
+
+    }
+
+    @Override
+    public void updated(String s) {
+
+    }
+}
